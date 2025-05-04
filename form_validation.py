58cf979d0ed9d0684790d11a206d11851fe57c2d
Antonio Dinkins <antonio.dinkins2019@gmail.com>
2025-05-03 20:22:26 -0400

    completed initial register form with form validation

--- form_validation.py
+++ form_validation.py
@@ -6,10 +6,10 @@ class LoginForm(Form):
 
 
 class RegistrationForm(Form):
- first_name = StringField('First Name:', [validators.length(min=2, max=25), validators.InputRequired()])
- last_name = StringField('Last Name:', [validators.length(min=2, max=25), validators.InputRequired()])
- email = EmailField('Email Address: ', [validators.length(min=10, max=25, message='Invalid email length'), validators.Email(message='Invalid email address'), validators.InputRequired()])
- password = PasswordField('Password: ', [validators.length(min=10), validators.EqualTo('confirm_password', message='Passwords do not match'), validators.InputRequired()])
+ first_name = StringField('First Name', [validators.length(min=2, max=25), validators.InputRequired()])
+ last_name = StringField('Last Name', [validators.length(min=2, max=25), validators.InputRequired()])
+ email = EmailField('Email Address', [validators.length(min=10, max=25, message='Invalid email length'), validators.Email(message='Invalid email address'), validators.InputRequired()])
+ password = PasswordField('Password', [validators.length(min=10), validators.EqualTo('confirm_password', message='Passwords do not match'), validators.InputRequired()])
  confirm_password = PasswordField('Reenter Password', [validators.length(min=10), validators.InputRequired()])
 
 
