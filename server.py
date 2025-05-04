a026b1ce19b9e6069401ec2658258ab9d6f0ddd3
Antonio Dinkins <antonio.dinkins2019@gmail.com>
2025-05-03 23:18:38 -0400

    Configured index.html in server.py and index route

--- server.py
+++ server.py
@@ -1,2 +1,16 @@
-from flask import Flask, session, flash, request
+from flask import Flask, session, flash, request, render_template
 from form_validation import RegistrationForm, LoginForm
+import os
+
+app = Flask('__name__')
+
+app['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+@app.route('/', methods=['GET'])
+def get_index():
+  return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port='5051')
