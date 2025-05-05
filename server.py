90debe779ba245942c837032bd9dce6d7abcac6f
Antonio Dinkins <antonio.dinkins2019@gmail.com>
2025-05-04 20:17:32 -0400

    Modified base.html to include main semantic tag and defined the general structure of the index.html page

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
-from flask import Flask, session, flash, request, render_template
+from flask import Flask, session, flash, request, render_template, redirect, jsonify
 from form_validation import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask('__name__')
+app.config('SQLALCHEMY_DATABASE_URI') = f"""
+  postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@/{os.environ.get('DB_NAME')}?host=/cloudsl/{os.environ.get('INSTANCE_CONNECTION_NAME')}
+  """
+
+db = SQLAlchemy(app)
 
 app['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 @app.route('/', methods=['GET'])
+@app.route('/home')
+@app.route('/index')
 def get_index():
   return render_template("index.html")
 
